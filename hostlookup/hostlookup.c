b4cac9316a088dd6c60f9350a1adf285d3d7a3a4
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-07-08 11:57:54 +1000

    batman commit
    import two old scripts

---
+++ hostlookup/hostlookup.c
@@ -0,0 +1,238 @@
+/*
+ * hostlookup.c
+ *
+ * Prints a lot of information about a hostname.
+ *
+ * Author: Matthew Kerwin <matthew.kerwin@qut.edu.au>
+ *
+ * Copyright (c) 2012-2016, QUT Library eServices <libsys@qut.edu.au>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#ifndef   NI_MAXHOST
+#define   NI_MAXHOST 1025
+#endif
+
+char* family(int af)
+{
+	switch (af) {
+	case AF_UNSPEC: return "UNSPEC";
+	case AF_LOCAL: return "LOCAL";
+/*	case AF_UNIX: return "UNIX";*/
+/*	case AF_FILE: return "FILE";*/
+	case AF_INET: return "INET";
+	case AF_AX25: return "AX25";
+	case AF_IPX: return "IPX";
+	case AF_APPLETALK: return "APPLETALK";
+	case AF_NETROM: return "NETROM";
+	case AF_BRIDGE: return "BRIDGE";
+	case AF_ATMPVC: return "ATMPVC";
+	case AF_X25: return "X25";
+	case AF_INET6: return "INET6";
+	case AF_ROSE: return "ROSE";
+	case AF_DECnet: return "DECnet";
+	case AF_NETBEUI: return "NETBEUI";
+	case AF_SECURITY: return "SECURITY";
+	case AF_KEY: return "KEY";
+	case AF_NETLINK: return "NETLINK";
+/*	case AF_ROUTE: return "ROUTE";*/
+	case AF_PACKET: return "PACKET";
+	case AF_ASH: return "ASH";
+	case AF_ECONET: return "ECONET";
+	case AF_ATMSVC: return "ATMSVC";
+	case AF_SNA: return "SNA";
+	case AF_IRDA: return "IRDA";
+	case AF_PPPOX: return "PPPOX";
+	case AF_WANPIPE: return "WANPIPE";
+	case AF_BLUETOOTH: return "BLUETOOTH";
+	case AF_MAX: return "MAX";
+	}
+	return "???";
+}
+
+char *stype(int st)
+{
+	switch (st) {
+	case SOCK_STREAM: return "SOCK_STREAM";
+	case SOCK_DGRAM: return "SOCK_DGRAM";
+	case SOCK_RAW: return "SOCK_RAW";
+	case SOCK_RDM: return "SOCK_RDM";
+	case SOCK_SEQPACKET: return "SOCK_SEQPACKET";
+	case SOCK_PACKET: return "SOCK_PACKET";
+	}
+	return "???";
+}
+
+char *sockop(int pf)
+{
+	switch (pf) {
+	case 0: return "SOL_IP";
+	case 1: return "SOL_ICMP";
+	case 6: return "SOL_TCP";
+	case 17: return "SOL_UDP";
+	case 41: return "SOL_IPV6";
+	case 58: return "SOL_ICMPV6";
+	case 255: return "SOL_RAW";
+	case 256: return "SOL_IPX";
+/* etc. */
+	case 263: return "SOL_PACKET";
+	}
+	return "???";
+}
+
+void printflags(unsigned int f)
+{
+	char c = '[';
+	if (!f) { printf("%c", c); }
+	if (f & AI_PASSIVE) { printf("%cAI_PASSIVE", c); c='|'; }
+	if (f & AI_CANONNAME) { printf("%cAI_CANONNAME", c); c='|'; }
+	if (f & AI_NUMERICHOST) { printf("%cAI_NUMERICHOST", c); c='|'; }
+	if (f & AI_V4MAPPED) { printf("%cAI_V4MAPPED", c); c='|'; }
+	if (f & AI_ALL) { printf("%cAI_ALL", c); c='|'; }
+	if (f & AI_ADDRCONFIG) { printf("%cAI_ADDRCONFIG", c); c='|'; }
+#ifdef __USE_GNU
+	if (f & AI_IDN) { printf("%cAI_IDN", c); c='|'; }
+	if (f & AI_CANONIDN) { printf("%cAI_CANONIDN", c); c='|'; }
+	if (f & AI_IDN_ALLOW_UNASSIGNED) { printf("%cAI_IDN_ALLOW_UNASSIGNED", c); c='|'; }
+	if (f & AI_IDN_USE_STD3_ASCII_RULES) { printf("%cAI_IDN_USE_STD3_ASCII_RULES", c); c='|'; }
+#endif
+	if (f & AI_NUMERICSERV) { printf("%cAI_NUMERICSERV", c); c='|'; }
+	printf("]\n");
+}
+
+char *myerr(int e)
+{
+	switch (e) {
+	case HOST_NOT_FOUND: return "HOST_NOT_FOUND";
+	case NO_ADDRESS: return "NO_ADDRESS";
+/*	case NO_DATA: return "NO_DATA";*/
+	case TRY_AGAIN: return "TRY_AGAIN";
+	default: return "???";
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct addrinfo  hints;
+	struct addrinfo *result;
+	struct addrinfo *res;
+	struct sockaddr_in *in;
+	struct hostent *host;
+	struct in_addr *ad;
+	int at;
+	int error;
+	int i;
+	char hostname[NI_MAXHOST];
+	char **alias;
+
+	for (i = 1; i < argc; i++) {
+		printf("***\n*** %s\n***\n\n", argv[i]);
+
+		/* super awesome hack bananas */
+		host = gethostbyname(argv[i]);
+		if (!host) {
+			fprintf(stderr, "error in gethostbyname: %s\n", myerr(h_errno));
+		} else {
+			printf("gethostbyname()\n  hostname: %s\n", host->h_name);
+			for (alias = host->h_aliases; alias && *alias; alias++) {
+				printf("  aka: %s\n", *alias);
+			}
+		}
+
+		/* resolve the domain name into a list of addresses */
+		memset(&hints, 0, sizeof(struct addrinfo));
+		hints.ai_family = AF_UNSPEC; /* AF_INET or AF_INET6 */
+#if 0
+		hints.ai_socktype = 0; /* SOCK_STREAM or SOCK_DGRAM */
+#endif
+		hints.ai_flags = AI_CANONNAME|AI_V4MAPPED;
+#if 0
+		hints.ai_protocol = 0; /* any protocol */
+		hints.ai_canonname = NULL;
+		hints.ai_addr = NULL;
+		hints.ai_next = NULL;
+#endif
+		error = getaddrinfo(argv[i], NULL, &hints, &result);
+		if (error != 0) {
+			fprintf(stderr, "error in getaddrinfo: %s\n", gai_strerror(error));
+			return EXIT_FAILURE;
+		}
+
+		printf("getaddrinfo()\n");
+
+		/* loop over all returned results and do inverse lookup */
+		for (res = result; res != NULL; res = res->ai_next) {
+			ad = NULL;
+
+			printf(">\n  ai_flags = 0x%X ", res->ai_flags);
+			printflags(res->ai_flags);
+			printf("  ai_family = %d [AF_%s]\n  ai_socktype = %d [%s]\n  ai_protocol = %d [%s]\n", res->ai_family, family(res->ai_family), res->ai_socktype, stype(res->ai_socktype), res->ai_protocol, sockop(res->ai_protocol));
+			if (res->ai_canonname && *(res->ai_canonname)) {
+				printf("  ai_canonname = \"%s\"\n", res->ai_canonname);
+			} else {
+				printf("  ai_canonname = NULL\n");
+			}
+			printf("  ai_addr = {\n");
+			if (res->ai_addrlen == sizeof(struct sockaddr_in)) {
+				in = (struct sockaddr_in*)(res->ai_addr);
+				printf("    sin_family = %d [AF_%s]\n    sin_port = %d\n    sin_addr = {\n", in->sin_family, family(in->sin_family), in->sin_port);
+				if (sizeof(in->sin_addr) == sizeof(struct in_addr)) {
+					ad = (struct in_addr*)&(in->sin_addr);
+					at = in->sin_family;
+					printf("      s_addr = 0x%08X (%d.%d.%d.%d)\n", ad->s_addr, ad->s_addr & 0xff, (ad->s_addr >> 8) & 0xff, (ad->s_addr >> 16) & 0xff, ad->s_addr >> 24);
+				} else {
+					printf("      ??? not an in_addr ???\n");
+				}
+				printf("    }\n");
+			} else {
+				printf("    ??? not a sockaddr_in ???\n");
+			}
+			printf("  }\n");
+
+			/* use new getnameinfo */
+			memset((void*)hostname, 0, NI_MAXHOST);
+			error = getnameinfo(res->ai_addr, res->ai_addrlen, hostname, NI_MAXHOST, NULL, 0, 0);
+			if (error != 0) {
+				fprintf(stderr, "error in getnameinfo: %s\n", gai_strerror(error));
+			}
+			if (*hostname)
+				printf("  getnameinfo(ai_addr)\n    hostname: %s\n", hostname);
+
+			/* use old gethostbyaddr */
+			if (ad) {
+				host = gethostbyaddr(ad, sizeof(ad), at);
+				if (!host) {
+					fprintf(stderr, "error in gethostbyaddr: %s\n", myerr(h_errno));
+				} else {
+					printf("  gethostbyaddr(ai_addr->sin_inaddr)\n    hostname: %s\n", host->h_name);
+					for (alias = host->h_aliases; alias && *alias; alias++) {
+						printf("  aka: %s\n", *alias);
+					}
+				}
+			}
+
+		}
+
+		freeaddrinfo(result);
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
+}
---
+++ passwdgen/passwdgen.c
@@ -0,0 +1,513 @@
+/*
+ * passwdgen.c
+ *
+ * Generates a pseudo-random password.
+ *
+ * Author:  Matthew Kerwin <matthew@kerwin.net.au>
+ *
+ * Copyright (C) 2009 Matthew Kerwin. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Matthew Kerwin.
+ * 4. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define BOLD   "\x1B[1m"
+#define NORMAL "\x1B[0m"
+
+const char* app_name = "passwdgen";
+const char* version  = "1.0";
+
+#define NO    0
+#define YES   1
+#define FORCE 2
+
+const char UPPER[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char LOWER[27] = "abcdefghijklmnopqrstuvwxyz";
+const char NUMER[11] = "0123456789";
+const char UPP_P[26] = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+const char LOW_P[26] = "abcdefghijkmnopqrstuvwxyz";
+const char NUM_P[9]  = "23456789";
+const char ASCII[33] = "`-=~!@#$%^&*()_+[]\\{}|;\':\",./<>?";
+
+/* OPTIONS */
+int repetitions = 5;
+int min_length = 16;
+int max_length = 32;
+int upper = FORCE;
+int lower = FORCE;
+int numer = FORCE;
+int ascii = NO;
+int printable = YES;
+
+char *custom_chars   = 0;
+int   custom_chars_n = 0;
+
+void parse_command_line(int argc, char *argv[]);
+void permute(char* string, int length);
+
+int main(int argc, char *argv[]) {
+
+	char *passwd = 0;
+	int   length = 0;
+
+	int has_upper = 0;
+	int has_lower = 0;
+	int has_numer = 0;
+	int has_ascii = 0;
+
+	int try_upper = 0;
+	int try_lower = 0;
+	int try_numer = 0;
+	int try_ascii = 0;
+
+	int i, j;
+
+	parse_command_line(argc, argv);
+
+	if ((upper | lower | numer | ascii) == 0) {
+		fprintf(stderr, "WARNING:  all characters disallowed.  Using default sets.\n");
+		upper = FORCE;
+		lower = FORCE;
+		numer = FORCE;
+		ascii = NO;
+	}
+
+	srandom(time(0));
+
+	for (j = 0; j < repetitions; j++) {
+		if (min_length == max_length) { length = max_length; }
+		else { length = random() % (max_length - min_length) + min_length; }
+		passwd = (char*) malloc(length + 1);
+		has_upper = 0;
+		has_lower = 0;
+		has_numer = 0;
+		has_ascii = 0;
+		for (i = 0; i < length; i++) {
+			if (custom_chars_n > 0) {
+				passwd[i] = custom_chars[random() % custom_chars_n];
+			} else {
+				if (upper == FORCE && !has_upper) { try_upper = 1; }
+				if (lower == FORCE && !has_lower) { try_lower = 2; }
+				if (numer == FORCE && !has_numer) { try_numer = 4; }
+				if (ascii == FORCE && !has_ascii) { try_ascii = 8; }
+				if ((try_upper | try_lower | try_numer | try_ascii) == 0) {
+					try_upper = (upper != NO) ? 1 : 0;
+					try_lower = (lower != NO) ? 2 : 0;
+					try_numer = (numer != NO) ? 4 : 0;
+					try_ascii = (ascii != NO) ? 8 : 0;
+				}
+				do {
+					switch (random() % 4) {
+						case 0:
+							if (try_upper) {
+								if (printable != NO) { passwd[i] = UPP_P[random() % 25]; }
+								else                 { passwd[i] = UPPER[random() % 26]; }
+								has_upper = 1;
+								goto ENDWHILE;
+							}
+							break;
+						case 1:
+							if (try_lower) {
+								if (printable != NO) { passwd[i] = LOW_P[random() % 25]; }
+								else                 { passwd[i] = LOWER[random() % 26]; }
+								has_lower = 1;
+								goto ENDWHILE;
+							}
+							break;
+						case 2:
+							if (try_numer) {
+								if (printable != NO) { passwd[i] = NUM_P[random() % 8]; }
+								else                 { passwd[i] = NUMER[random() % 10]; }
+								has_numer = 1;
+								goto ENDWHILE;
+							}
+							break;
+						case 3:
+							if (try_ascii) {
+								passwd[i] = ASCII[random() % 32];
+								has_ascii = 1;
+								goto ENDWHILE;
+							}
+							break;
+						default:
+							break;
+					}
+				} while (1);
+ENDWHILE:
+				;
+			}
+		}
+		passwd[length] = 0;
+		permute(passwd, length);
+		printf("%s\n", passwd);
+		free(passwd);
+	}
+
+	return 0;
+}
+
+void show_version(void) {
+	printf("%s (v%s)\n", app_name, version);
+}
+
+void show_help(void) {
+	printf("%sVERSION:%s\n", BOLD, NORMAL);
+	show_version();
+	printf("%sOPTIONS:%s\n", BOLD, NORMAL);
+	printf(" %s-C%s #  %s-c%s #  %s--count%s=#\n   generate a fixed number of passwords.  Default is 5.\n", BOLD, NORMAL, BOLD, NORMAL, BOLD, NORMAL);
+	printf(" %s-N%s #  %s-n%s #  %s--min%s=#\n %s-X%s #  %s-x%s #  %s--max%s=#\n", BOLD, NORMAL, BOLD, NORMAL, BOLD, NORMAL, BOLD, NORMAL, BOLD, NORMAL, BOLD, NORMAL);
+	printf("   passwords have the given minimum/maximum length.  Defaults are 16, 32.\n");
+	printf(" %s--%s [CHARACTERS]\n   passwords are built using precisely the given set of characters.\n", BOLD, NORMAL);
+	printf(" %s[+-=]U  [+-=]u  --upper%s=[YES|NO|FORCE]\n", BOLD, NORMAL);
+	printf(" %s[+-=]L  [+-=]l  --lower%s=[YES|NO|FORCE]\n", BOLD, NORMAL);
+	printf(" %s[+-=]D  [+-=]d  --digits%s=[YES|NO|FORCE]\n", BOLD, NORMAL);
+	printf(" %s[+-=]A  [+-=]a  --ascii%s=[YES|NO|FORCE]\n", BOLD, NORMAL);
+	printf("   accept/deny/force upper case characters, lower case characters, decimal digits,\n");
+	printf("   and non-alphanumeric ASCII characters (respectively) in passwords.\n");
+	printf("   Defaults are: %s=ULD-A%s\n", BOLD, NORMAL);
+	printf("   Ignored if %s-- characters%s is specified.\n", BOLD, NORMAL);
+	printf(" %s[+-]P  [+-]p  --printable%s=[YES|NO]\n", BOLD, NORMAL);
+	printf("   only generate printable passwords (no 1,l,O,0 characters).  Default = %sYES%s\n", BOLD, NORMAL);
+	printf("   Ignored if %s-- characters%s is specified.\n", BOLD, NORMAL);
+	printf(" %s-V%s  %s-v%s  %s--version%s\n   show version information, and quit.\n", BOLD, NORMAL, BOLD, NORMAL, BOLD, NORMAL);
+	printf(" %s-?%s  %s--help%s\n   show this help information, and quit.\n", BOLD, NORMAL, BOLD, NORMAL);
+}
+
+void bad_parameter(const char* param) {
+	fprintf(stderr, "Unrecognised parameter: %s\n", param);
+	exit(1);
+}
+
+void bad_parameter_token(const char* param, char token) {
+	fprintf(stderr, "Unrecognised token %c in parameter: %s\n", token, param);
+	exit(2);
+}
+
+void parameter_missing_argument(const char* param) {
+	fprintf(stderr, "Parameter missing argument: %s\n", param);
+	exit(3);
+}
+
+int starts_with(const char* long_str, const char* short_str) {
+	char *sp = (char*)short_str;
+	char *lp = (char*)long_str;
+	do {
+		if (*sp == *lp) {
+			++ sp;
+			++ lp;
+		} else {
+			return 0;
+		}
+	} while (*sp != 0);
+	return 1;
+}
+
+void get_number(int* var, const char* arg, const char* message) {
+	int n = atoi(arg);
+	if (n < 1) {
+		fprintf(stderr, "Invalid %s: %s\n", message, arg);
+		exit(4);
+	}
+	*var = n;
+}
+
+void get_accept(int* var, const char* arg, const char* message) {
+	if (strcmp(arg, "NO" )   == 0) {*var = NO;    return;}
+	if (strcmp(arg, "YES")   == 0) {*var = YES;   return;}
+	if (strcmp(arg, "FORCE") == 0) {*var = FORCE; return;}
+	if (strcmp(arg, "no" )   == 0) {*var = NO;    return;}
+	if (strcmp(arg, "yes")   == 0) {*var = YES;   return;}
+	if (strcmp(arg, "force") == 0) {*var = FORCE; return;}
+	fprintf(stderr, "Invalid %s: %s\nExpected YES, NO, or FORCE\n", message, arg);
+	exit(5);
+}
+
+void get_boolean(int* var, const char* arg, const char* message) {
+	if (strcmp(arg, "NO" )   == 0) {*var = 0; return;}
+	if (strcmp(arg, "YES")   == 0) {*var = 1; return;}
+	if (strcmp(arg, "no" )   == 0) {*var = 0; return;}
+	if (strcmp(arg, "yes")   == 0) {*var = 1; return;}
+	fprintf(stderr, "Invalid %s: %s\nExpected YES, NO\n", message, arg);
+	exit(6);
+}
+
+void get_custom_chars(const char* arg, const char* message) {
+	int n;
+	if (custom_chars != 0) {
+		free(custom_chars);
+	}
+	n = strlen(arg);
+	if (n != 0) {
+		custom_chars_n = n;
+		custom_chars   = (char*) malloc(custom_chars_n);
+		strcpy(custom_chars, arg);
+	} else {
+		fprintf(stderr, "WARNING: blank %s specified.  Using default.\n", message);
+	}
+}
+
+inline void consume_chained_params(const char *arg, const char *original_arg, int accept) {
+	char *ptr = (char*) arg;
+	int got_chars = 0;
+	for (; *ptr != 0; ptr ++) {
+		switch (*ptr) {
+			case 'U':
+			case 'u':
+				upper = accept;
+				break;
+			case 'L':
+			case 'l':
+				lower = accept;
+				break;
+			case 'D':
+			case 'd':
+				numer = accept;
+				break;
+			case 'A':
+			case 'a':
+				ascii = accept;
+				break;
+			case 'P':
+			case 'p':
+				if (accept == FORCE) {
+					bad_parameter_token(original_arg, *ptr);
+				} else {
+					printable = accept;
+				}
+				break;
+			case '+':
+				if (got_chars == 0) {
+					bad_parameter(original_arg);
+				} else {
+					consume_chained_params(ptr + 1, original_arg, YES);
+				}
+				return;
+			case '=':
+				if (got_chars == 0) {
+					bad_parameter(original_arg);
+				} else {
+					consume_chained_params(ptr + 1, original_arg, FORCE);
+				}
+				return;
+			case '-':
+				if (got_chars == 0) {
+					bad_parameter(original_arg);
+				} else {
+					consume_chained_params(ptr + 1, original_arg, NO);
+				}
+				return;
+			default:
+				bad_parameter_token(original_arg, *ptr);
+		}
+		got_chars = 1;
+	}
+}
+
+void parse_command_line(int argc, char *argv[]) {
+	int debug = 0;
+	int i,j;
+
+	i = 1;
+	while (i < argc) {
+		switch (argv[i][0]) {
+			case '+':
+				consume_chained_params(argv[i] + 1, argv[i], YES);
+				goto NEXT_ARG;
+			case '=':
+				consume_chained_params(argv[i] + 1, argv[i], FORCE);
+				goto NEXT_ARG;
+			case '-':
+				switch (argv[i][1]) {
+					case 'U':
+					case 'u':
+					case 'L':
+					case 'l':
+					case 'D':
+					case 'd':
+					case 'A':
+					case 'a':
+					case 'P':
+					case 'p':
+						consume_chained_params(argv[i] + 1, argv[i], NO);
+						goto NEXT_ARG;
+					case 'C':
+					case 'c':
+						if (argv[i][2] != 0) {
+							bad_parameter(argv[i]);
+						}
+						if (argc <= i + 1) {
+							parameter_missing_argument(argv[i]);
+						}
+						++ i;
+						get_number(&repetitions, argv[i], "count");
+						goto NEXT_ARG;
+					case 'X':
+					case 'x':
+						if (argv[i][2] != 0) {
+							bad_parameter(argv[i]);
+						}
+						if (argc <= i + 1) {
+							parameter_missing_argument(argv[i]);
+						}
+						++ i;
+						get_number(&max_length, argv[i], "maximum length");
+						goto NEXT_ARG;
+					case 'N':
+					case 'n':
+						if (argv[i][2] != 0) {
+							bad_parameter(argv[i]);
+						}
+						if (argc <= i + 1) {
+							parameter_missing_argument(argv[i]);
+						}
+						++ i;
+						get_number(&min_length, argv[i], "minimum length");
+						goto NEXT_ARG;
+					case '?':
+						if (argv[i][2] != 0) {
+							bad_parameter(argv[i]);
+						}
+						show_help();
+						exit(0);
+					case 'V':
+					case 'v':
+						if (argv[i][2] != 0) {
+							bad_parameter(argv[i]);
+						}
+						show_version();
+						exit(0);
+					case '-':
+						if (argv[i][2] == 0) {
+							if (argc <= i + 1) {
+								parameter_missing_argument(argv[i]);
+							}
+							++ i;
+							get_custom_chars(argv[i], "custom characters");
+							goto NEXT_ARG;
+						} else if (strcmp(argv[i] + 2, "help") == 0) {
+							show_help();
+							exit(0);
+						} else if (strcmp(argv[i] + 2, "version") == 0) {
+							show_version();
+							exit(0);
+						} else if (starts_with(argv[i] + 2, "count=")) {
+							get_number(&repetitions, argv[i] + 8, "count");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "max=")) {
+							get_number(&max_length, argv[i] + 6, "maximum length");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "maximum=")) {
+							get_number(&max_length, argv[i] + 10, "maximum length");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "min=")) {
+							get_number(&min_length, argv[i] + 6, "minimum length");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 1, "minimum=")) {
+							get_number(&min_length, argv[i] + 10, "minimum length");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "upper=")) {
+							get_accept(&upper, argv[i] + 8, "upper");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "upper-case=")) {
+							get_accept(&upper, argv[i] + 13, "upper-case");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "lower=")) {
+							get_accept(&lower, argv[i] + 8, "lower");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "lower-case=")) {
+							get_accept(&lower, argv[i] + 13, "lower-case");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "digits=")) {
+							get_accept(&numer, argv[i] + 9, "digits");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "decimal=")) {
+							get_accept(&numer, argv[i] + 10, "decimal");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "ascii=")) {
+							get_accept(&ascii, argv[i] + 8, "ascii");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "print=")) {
+							get_boolean(&printable, argv[i] + 8, "printable");
+							goto NEXT_ARG;
+						} else if (starts_with(argv[i] + 2, "printable=")) {
+							get_boolean(&printable, argv[i] + 12, "printable");
+							goto NEXT_ARG;
+						} else if (strcmp(argv[i] + 2, "debug") == 0) {
+							debug = 1;
+							goto NEXT_ARG;
+						}
+						/*fall through*/
+					default:
+						;
+						/*fall through*/
+				}
+			default:
+				bad_parameter(argv[i]);
+				/*fall through*/
+		}
+NEXT_ARG:
+		++ i;
+	}
+
+	if (debug) {
+		show_help();
+		printf("%sOPTIONS:%s\n", BOLD, NORMAL);
+		printf("  -C %d\n", repetitions);
+		printf("  -X %d\n", max_length);
+		printf("  -N %d\n", min_length);
+		if (custom_chars_n > 0) {
+			printf("  -- \"%s\"\n", custom_chars);
+		} else {
+			printf("  -- \"\"\n");
+		}
+		printf("  %cU\n", upper == NO ? '-' : (upper == FORCE ? '=' : '+'));
+		printf("  %cL\n", lower == NO ? '-' : (lower == FORCE ? '=' : '+'));
+		printf("  %cD\n", numer == NO ? '-' : (numer == FORCE ? '=' : '+'));
+		printf("  %cA\n", ascii == NO ? '-' : (ascii == FORCE ? '=' : '+'));
+		printf("  %cP\n", printable == NO ? '-' : '+');
+		exit(0);
+	}
+}
+
+void permute(char* string, int length) {
+	int i, j;
+	char c;
+	if (length < 2) return;
+	for (i = 0; i < length; i++) {
+		j = random() % length;
+		if (j == i) {
+			j = (j + 1) % length;
+		}
+		c = string[j];
+		string[j] = string[i];
+		string[i] = c;
+	}
+}
